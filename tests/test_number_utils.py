c41e4189ee4bae997dc7e3dd2e8cbb4ba2ab1977
Manattee Vilairat <6810110275@psu.ac.th>
2026-03-06 13:12:49 +0700

    added: one more test case [2, 3, 5]

--- tests/test_number_utils.py
+++ tests/test_number_utils.py
@@ -6,3 +6,8 @@ class PrimeListTest(unittest.TestCase):
         prime_list = [1, 2, 3]
         is_prime = is_prime_list(prime_list)
         self.assertTrue(is_prime)
+
+    def test_give_2_3_5_is_prime(self):
+        prime_list = [2, 3, 5]
+        is_prime = is_prime_list(prime_list)
+        self.assertTrue(is_prime)
