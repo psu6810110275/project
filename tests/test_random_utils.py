d2589a13aa2112f0dccbaa802e4e5713530e7ae9
Manattee Vilairat <6810110275@psu.ac.th>
2026-03-06 14:55:23 +0700

    Test double

---
+++ tests/test_random_utils.py
@@ -0,0 +1,32 @@
+import unittest
+from unittest.mock import patch
+from coe_6810110275.random_utils import guess_int, guess_float
+
+class RandomUtilsTest(unittest.TestCase):
+    
+    @patch('coe_6810110275.random_utils.random.randint')
+    def test_guess_int_should_return_mocked_value(self, mock_randint):
+        mock_randint.return_value = 5
+        start, stop = 1, 10
+        expected_output = 5
+        
+        # Act
+        result = guess_int(start, stop)
+        
+        # Assert
+        self.assertEqual(result, expected_output)
+        mock_randint.assert_called_once_with(start, stop)
+
+    @patch('coe_6810110275.random_utils.random.uniform')
+    def test_guess_float_should_return_mocked_value(self, mock_uniform):
+        # Arrange
+        mock_uniform.return_value = 3.14
+        start, stop = 1.0, 5.0
+        expected_output = 3.14
+        
+        # Act
+        result = guess_float(start, stop)
+        
+        # Assert
+        self.assertEqual(result, expected_output)
+        mock_uniform.assert_called_once_with(start, stop)
